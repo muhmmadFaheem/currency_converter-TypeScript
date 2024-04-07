2a7e19f19c7e4661af8f4517e3f9c865e28335f8
Muhammad Faheem <157746418+muhmmadFaheem@users.noreply.github.com>
2024-04-07 16:15:13 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const currency :any = {
+    USD : 1,
+    PKR : 280,
+    KRW : 1350.23,
+    QAR : 3.62,
+    GBP : 1.26,
+    EUR : 0.86
+
+}
+
+let answer = await inquirer.prompt([{
+    name : "from",
+    type : "list",
+    message : "Enter From Currency",
+    choices : ["USD", "PKR", "GBP", "EUR", "QAR", "KRW"]
+    
+},
+{
+    name : "to",
+    type : "list",
+    message : "Enter to Currency",
+    choices : ["USD", "PKR", "GBP", "EUR", "QAR", "KRW"]
+
+},
+{
+    name : "amount",
+    type : "input",
+    message :"Enter Amount For Convert",
+},
+
+]);
+
+let  fromAmount = currency[answer.from];
+let toAmount = currency[answer.to];
+let amount = answer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+
+console.log("Converted Amount of Currency : " + chalk.redBright(convertedAmount));
+
